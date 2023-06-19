6c947f1353d06b6d269f64e3a509dc78aff7e7d4
Eric Njanga <eric.njanga@gmail.com>
2023-06-19 18:26:36 -0400

    Bottom page CTA button styling

--- template-service.php
+++ template-service.php
@@ -32,19 +32,21 @@
         <?php echo getFieldByID('how_we_can_help', get_the_ID()); ?>
     </blockquote>
 
-    <footer>
+    <footer class="cta-btn-container">
         <?php
             // Page and post category have identical slugs, 
             // this makes it easy to link them
             $current_slug = get_post_field('post_name', get_queried_object_id());
             $blogLink = getSubCategoryLink($current_slug, 'category');
         ?>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#appointmentModal">
-            Schedule an appointment
-        </button>
-        <a href="<?php echo $blogLink; ?>" class="btn btn-secondary">
-            Additional reading
-        </a>
+        <p>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#appointmentModal">
+                Schedule an appointment
+            </button>
+            <a href="<?php echo $blogLink; ?>" class="btn btn-secondary">
+                Additional reading
+            </a>
+        </p>
     </footer>
 </main>
 
