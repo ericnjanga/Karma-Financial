1669bf3f7ca6362d4b51f73a58c0a571319e5b47
Eric Njanga <eric.njanga@gmail.com>
2023-06-19 15:46:31 -0400

    Updated meta-navigation font family

--- entry-footer.php
+++ entry-footer.php
@@ -1,5 +1,20 @@
+
 <footer class="entry-footer">
-<span class="cat-links"><?php esc_html_e( 'Categories: ', 'generic' ); ?><?php the_category( ', ' ); ?></span>
+    <span class="cat-links">
+        <?php esc_html_e( 'Categories: ', 'generic' ); ?>
+    </span>
+    <?php
+        $categories = get_the_category();
+        if (!empty($categories)) {
+            echo '<span class="heading-ff fs-6">';
+            foreach ($categories as $category) {
+                echo '<a href="' . esc_url(get_category_link($category->term_id)) . '">' . esc_html($category->name) . '</a>';
+            }
+            echo '</span>';
+        }
+    ?>
+
+
 <span class="tag-links"><?php the_tags(); ?></span>
 <?php //if ( comments_open() ) { echo '<span class="meta-sep">|</span> <span class="comments-link"><a href="' . esc_url( get_comments_link() ) . '">' . sprintf( esc_html__( 'Comments', 'generic' ) ) . '</a></span>'; } ?>
 </footer>
